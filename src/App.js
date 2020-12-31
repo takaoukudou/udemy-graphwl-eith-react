a77517837092942be5f4ca84385b67ab0c5ae550
工藤貴央 <takaoukudou@macbook.local>
2020-12-31 17:52:16 +0900

    set graphql

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
-function App() {
-	return <div>Hello world</div>;
+import client from "./client";
+import { ApolloProvider } from "react-apollo";
+import { render } from "react-dom";
+import { Component } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const ME = gql`
+  query me {
+    user(login:'takaoukudou'){
+      name
+      avatarUrl
+    }
+  }
+`;
+
+class App extends Component {
+	render() {
+		return (
+			<ApolloProvider client={client}>
+				<div>Hello world</div>
+				<Query query={ME}>
+					{({ loading, error, data }) => {
+						if (loading) return "Loading...";
+						if (error) return `Error! ${error.message}`;
+
+						return <div>{data.user.name}</div>;
+					}}
+				</Query>
+			</ApolloProvider>
+		);
+	}
 }
 
 export default App;
